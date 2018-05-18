621b5eff90439bca200c2199b84dd2d69f41e603
Jay Lord Torres <jaylord@globalbrainforce.com>
2018-05-18 09:16:03 +0800

    some info

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -6,7 +6,7 @@ import StartupActions from '../Redux/StartupRedux'
 import ReduxPersist from '../Config/ReduxPersist'
 import BusyIndicator from 'react-native-busy-indicator'
 
-import { Root } from "native-base"
+import { Root } from 'native-base'
 
 // Styles
 import styles from './Styles/RootContainerStyles'
@@ -29,7 +29,7 @@ class RootContainer extends Component {
     )
   }
 }
- 
+
 // wraps dispatch to create nicer functions to call within our component
 const mapDispatchToProps = (dispatch) => ({
   startup: () => dispatch(StartupActions.startup())
