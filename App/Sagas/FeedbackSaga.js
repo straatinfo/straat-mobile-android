a37f6e795ebefe56e52b041ed99f285ad4532005
Jay Lord Torres <jaylord@globalbrainforce.com>
2018-05-20 22:14:37 +0800

    clean: FeedbackSaga.js

--- App/Sagas/FeedbackSaga.js
+++ App/Sagas/FeedbackSaga.js
@@ -1,33 +1,22 @@
-import loaderHandler from 'react-native-busy-indicator/LoaderHandler'
 import FeedbackActions from '../Redux/FeedbackRedux'
-import { showAlertBox, logStore, AppData } from './../Redux/commonRedux'
-import { put, call, select } from 'redux-saga/effects'
-import { changeto } from '../Redux/ScreenRedux'
-
-import { popUpAlert } from './../Lib/Helper/alertHelper'
-import { onloginPopUp, getApprovedTeamList } from './../Transforms/Filters'
+import loaderHandler from 'react-native-busy-indicator/LoaderHandler'
 import { getLanguageState } from './../Redux/LanguageRedux'
+import { popUpAlert } from './../Lib/Helper/alertHelper'
+import { put, call, select } from 'redux-saga/effects'
+import { showAlertBox} from './../Redux/commonRedux'
+
 export const sendFeedback = function * (API, action) {
-    const language = yield select(getLanguageState)
-    try {
-        console.log('Sending feedback...', action);
-        console.log('Sending feedback API', API);
-
-        // show loader
-        yield call(loaderHandler.showLoader, language.txt_C08)
-
-        // api call
-        const feedbackResponse = yield call(API.postFeedback, action.feedback);
-        if (feedbackResponse.ok && feedbackResponse.data.status === 1) {
-            console.log('Sending feedback successful.', feedbackResponse);
-            yield put(FeedbackActions.sendFeedbackSuccess());
-            yield call(popUpAlert, { title: ' ', message: feedbackResponse.data.data.message, pressok: () => {} })
-        }
-    } catch (error) {
-        console.log('Sending feedback failed. ', error);
-        yield put(FeedbackActions.sendFeedbackFailure(error));
-        yield call(showAlertBox, error.message);
+  const language = yield select(getLanguageState)
+  try {
+    yield call(loaderHandler.showLoader, language.txt_C08)
+    const feedbackResponse = yield call(API.postFeedback, action.feedback)
+    if (feedbackResponse.ok && feedbackResponse.data.status === 1) {
+      yield put(FeedbackActions.sendFeedbackSuccess())
+      yield call(popUpAlert, { title: ' ', message: feedbackResponse.data.data.message, pressok: () => {} })
     }
-    // clean screen
-    yield call(loaderHandler.hideLoader)
+  } catch (error) {
+    yield put(FeedbackActions.sendFeedbackFailure(error))
+    yield call(showAlertBox, error.message)
+  }
+  yield call(loaderHandler.hideLoader)
 }
