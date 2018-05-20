66dcb46943ababab01d7f7c3d0aa28476d667cd1
Jay Lord Torres <jaylord@globalbrainforce.com>
2018-05-20 22:47:01 +0800

    clean: StartupSagas.js

--- App/Sagas/StartupSagas.js
+++ App/Sagas/StartupSagas.js
@@ -1,16 +1,13 @@
-import { put, select, call } from 'redux-saga/effects'
 import GithubActions, { GithubSelectors } from '../Redux/GithubRedux'
-import { is } from 'ramda'
-import { AppData } from '../Redux/commonRedux'
-
-import UserActions, { setTheme } from './../Redux/UserRedux'
 import LanguageActions from './../Redux/LanguageRedux'
+import UserActions, { setTheme } from './../Redux/UserRedux'
+import { AppData } from '../Redux/commonRedux'
 import { designDefault } from '../Transforms/themeHelper'
+import { is } from 'ramda'
+import { put, select, call } from 'redux-saga/effects'
 
-// exported to make available for tests
 export const selectAvatar = GithubSelectors.selectAvatar
 
-// process STARTUP actions
 export const startup = function * (action) {
   if (__DEV__ && console.tron) {
     // straight-up string logging
@@ -38,39 +35,30 @@ export const startup = function * (action) {
     })
   }
   const avatar = yield select(selectAvatar)
-  // only get if we don't have it yet
   if (!is(String, avatar)) {
     yield put(GithubActions.userRequest('GantMan'))
   }
 
   // configure app
-
 }
 
-
-
 export const configureApp = function * (action) {
-
   try {
     // set theam base on old user
     const theme = yield call(AppData.getTheme)
     const design = JSON.parse(theme)
-    __DEV__ && console.log('AppData.getTheme', design)
     if (design !== null && design.button !== undefined) {
       yield call(setTheme, design)
       yield put(UserActions.mergeState({design: design}))
     }
-  
+
     // set up language base on old user
     const userInfo = yield call(AppData.getUserInfo)
     const oldUser = JSON.parse(userInfo)
-    __DEV__ && console.log('initializing language')
     if (oldUser.language) {
       yield put(LanguageActions.setLanguage(oldUser.language))
     }
   } catch (e) {
     yield call(setTheme, designDefault)
-    // yield put(UserActions.mergeState({design: designDefault}))
   }
 }
-
