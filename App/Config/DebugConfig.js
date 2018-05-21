dd38129254287d3d4bc6bba6e7ee97eb90514024
Jay Lord Torres <jaylord@globalbrainforce.com>
2018-05-21 15:13:15 +0800

    config: add displayGoogleMap to debugConfig.js

--- App/Config/DebugConfig.js
+++ App/Config/DebugConfig.js
@@ -2,10 +2,12 @@ export default {
   showDevScreens: __DEV__,
   useFixtures: false,
   ezLogin: false,
-  yellowBox: __DEV__,
+  yellowBox: false,  // __DEV__,
   reduxLogging: __DEV__,
   includeExamples: __DEV__,
   useReactotron: true,
   displayEpiredReportsInMap: false,
-  displayNotificationCountOfHisReport: false
+  displayNotificationCountOfHisReport: false,
+  displayGoogleMap: true
 }
+ 
