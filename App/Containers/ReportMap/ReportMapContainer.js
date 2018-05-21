0e8353c4de041f6f4ce5b25e963d3bba363f3800
Jay Lord Torres <jaylord@globalbrainforce.com>
2018-05-21 15:02:11 +0800

    reportmap: add map disable option for debugmode

--- App/Containers/ReportMap/ReportMapContainer.js
+++ App/Containers/ReportMap/ReportMapContainer.js
@@ -44,6 +44,8 @@ import ReportStyle from './../../Components/ReportDashboard/ReportStyle.js'
 import styles from './style'
 import { reportCoordinate, getReportsNearbyRequest } from '../../Redux/ReportsRedux'
 import { cropWH } from '../../Transforms/Cloudinary'
+import { displayGoogleMap } from '../../Config/DebugConfig'
+
 /**
  * i think i will not shift this module to redux saga, as of now i dont have time for that @ArC
  *
@@ -466,6 +468,9 @@ class ReportMapContainer extends Component {
     const confirmChangeStatus = this.confirmChangeStatus.bind(this)
     const pinImage = this.pinImage
     const heights = this.getHeight()
+    if (!displayGoogleMap) {
+      return null
+    }
     return (
       <View style={styles.container}>
         {renderIf(this.state.mapReset === 1)(
