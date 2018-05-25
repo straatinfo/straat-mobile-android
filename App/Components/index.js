4bc35a66423653899fefbdd3db7f845b52d1b6c9
Jay Lord Torres <isens.jaylord@gmail.com>
2018-05-26 03:53:50 +0800

    feature: component: index

---
+++ App/Components/index.js
@@ -0,0 +1,13 @@
+import BannerLogoComponent from './BannerLogo'
+import ButtonComponent from './Button'
+import SpacerComponent from './Spacer'
+import AlertBoxComponent from './AlertBox'
+
+
+export class AlertBox extends AlertBoxComponent {}
+
+export class BannerLogo extends BannerLogoComponent {}
+
+export class Button extends ButtonComponent {}
+
+export class Spacer extends SpacerComponent {}
