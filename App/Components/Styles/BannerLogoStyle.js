a80127c3332d3e916cb792e2ea7e2b5450239afd
Jay Lord Torres <isens.jaylord@gmail.com>
2018-05-26 03:53:27 +0800

    feature: component: BannerLogo

---
+++ App/Components/Styles/BannerLogoStyle.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    // flex: 1
+  },
+
+  upperbox: {
+    height: 150,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFFFFF'
+  },
+  logoHolder: {
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+
+  logo: {
+    width: 98,
+    height: 98,
+    alignSelf: 'center'
+  //  resizeMode: 'contain'
+  }
+})
