4045b38b1f04108d87811cd3c607f0bee9382d99
Jay Lord Torres <isens.jaylord@gmail.com>
2018-06-07 20:52:21 +0800

    compenent: updated

--- App/Components/index.js
+++ App/Components/index.js
@@ -1,17 +1,28 @@
-import BannerLogoComponent from './BannerLogo'
-import CircleLoaderComponent from './CircleLoader'
-import ButtonComponent from './Button'
-import SpacerComponent from './Spacer'
-import AlertBoxComponent from './AlertBox'
+import LinearGradientComponent from 'react-native-linear-gradient'
 
+import AlertBoxComponent from './AlertBox'
+import BannerLogoComponent from './BannerLogo'
+import CenterViewComponent from './CenterView'
+import CircleLoaderComponent from './CircleLoader'
+import CircleButtonComponent from './CircleButton'
+import ButtonComponent from './Button'
+import RowViewComponent from './RowView'
+import SpacerComponent from './Spacer'
 
 export class AlertBox extends AlertBoxComponent {}
-
-export class CircleLoader extends CircleLoaderComponent {}
 
 export class BannerLogo extends BannerLogoComponent {}
 
 export class Button extends ButtonComponent {}
 
-export class Spacer extends SpacerComponent {}
+export class CenterView extends CenterViewComponent {}
 
+export class CircleButton extends CircleButtonComponent {}
+
+export class CircleLoader extends CircleLoaderComponent {}
+
+export class LinearGradient extends LinearGradientComponent {}
+
+export class RowView extends RowViewComponent {}
+
+export class Spacer extends SpacerComponent {}
