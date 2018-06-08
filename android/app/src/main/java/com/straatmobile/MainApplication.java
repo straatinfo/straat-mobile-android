18a7706a8ef26d6682943d7d1ddc4911cf01fdea
Jay Lord Torres <isens.jaylord@gmail.com>
2018-06-09 04:08:32 +0800

    package: react-native-push-notification

--- android/app/src/main/java/com/straatmobile/MainApplication.java
+++ android/app/src/main/java/com/straatmobile/MainApplication.java
@@ -3,6 +3,7 @@ package com.straatmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.airbnb.android.react.maps.MapsPackage;
@@ -34,6 +35,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new LinearGradientPackage(),
             new RNSpinkitPackage(),
             new MapsPackage(),
